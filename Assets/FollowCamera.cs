de7ce26120ef397af61f2271e0a8fc03f6b1b704
jercon115 <jeremi.concepcion@gmail.com>
2015-06-24 22:27:16 -0500

    Made camera follow player and changed test planet

---
+++ Assets/FollowCamera.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCamera : MonoBehaviour {
+
+	public GameObject target;
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.Translate((target.transform.localPosition - transform.localPosition ) * 0.1f);
+		transform.Translate(new Vector3(0.0f,0.0f, -10 - transform.localPosition.z));
+	}
+}
